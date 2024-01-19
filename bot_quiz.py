1ec4c6ad5f0ad2a1c738f6fc0fec225ef05659dd
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-01-19 22:26:45 +0100

    test-0.2

--- bot_quiz.py
+++ bot_quiz.py
@@ -1,10 +1,20 @@
 import os
 import discord
+from discord import app_commands as apc
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+tree = apc.CommandTree(client)
+
+@tree.command(
+    name="hello",
+    description="Say hello to the bot",
+)
+async def hello(interaction):
+    await interaction.response.send_message("Hello!")
 
 @client.event
 async def on_ready():
@@ -15,7 +25,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send(message.content)
         
 # On récupère notre token discord dans l'env de Railway
